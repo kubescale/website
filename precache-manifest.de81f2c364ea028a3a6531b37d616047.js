37fc6075d866a651a8eacd3f18ad8d4902d8fa2b
toonsevrin <twan123@live.be>
2020-07-02 19:10:47 +0200

    Updates

--- precache-manifest.de81f2c364ea028a3a6531b37d616047.js
+++ precache-manifest.de81f2c364ea028a3a6531b37d616047.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "63c17bd06e7e94d83fed8f4f032fec73",
+    "revision": "fe9a1a20544b3628135bdf7ef68f5a19",
     "url": "./index.html"
   },
   {
